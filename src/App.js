ba23d35063f19147a758ad440ebbd56913d3a5f7
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-12 17:18:33 +0000

    todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      userInput: '',
+      list: []
+    }
+  }
+
+  changeUserInput(input) {
+    this.setState({
+      userInput: input
+    });
+  }
+
+  addToList(input) {
+    let listArray = this.state.list
+
+    listArray.push(input);
+
+    this.setState({
+      list: listArray,
+      userInput: ''
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          onChange={ (e)=>this.changeUserInput(e.target.value) }
+          value={this.state.userInput}
+          type="text" />
+        <button onClick={ ()=> this.addToList(this.state.userInput)}>press me</button>
+        <ul>
+          {this.state.list.map((value)=> <li>{value}</li>)}
+        </ul>
+      </div>
+    );
+  }
+
 }
 
 export default App;
